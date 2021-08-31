fded80d352f2297af0f98ab1b8a272f22d0ffa93
Tamer545 <8235.Lohn>
2021-08-31 08:26:23 +0200

    redirect

--- src/project/Registration.js
+++ src/project/Registration.js
@@ -19,6 +19,7 @@ export default function Registration() {
             storeUser()
             console.log(username)
             validationStatus  = 1
+
         }else{
             validationStatus = 0
         }
